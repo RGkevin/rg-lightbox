e896669b455304b7d271b9bc5f6306a30dcdd15e
Kevin Lopez <rebeliongraficks@gmail.com>
2013-10-05 05:21:11 -0600

    resize method

--- rg-lightbox.js
+++ rg-lightbox.js
@@ -6,163 +6,165 @@
  * Copyright (C) 2013 @RGkevin
  */
 
-(function(root) {
+(function(root, factory) {
 
-	var _rglightbox = function() {
-		'use strict';
+	/* CommonJS */
+  if (typeof exports == 'object')  module.exports = factory()
 
-		// # build object
+  /* AMD module */
+  else if (typeof define == 'function' && define.amd) define(['spin'], factory)
 
-		var options = {
-			transition : 350
-		}
+  /* Browser global */
+  else root.RGlightbox = factory( root.Spinner )
 
-		// define data
+})
+(this, function(Spinner) {
+	'use strict';
 
-		this.__defineGetter__("fading", function(){
-			var _lb = this._build(),
-			_lbStyles = window.getComputedStyle(_lb),
-			_transition = parseFloat(_lbStyles.getPropertyValue('transition-duration') || '0.0s' ) * 1000;
-			console.log('se quiere accesar al dato fading', _transition);
-			return _transition;
-		});
+	/**
+   * Fills in default values.
+   */
+  function merge(obj) {
+  	for (var i = 1, len = arguments.length; i < len; i++) {
+      var def = arguments[i]
+      for (var n in def)
+        if (obj[n] === undefined) obj[n] = def[n]
+    }
+    return obj
+  }
 
-		this._init();
+  // Built-in defaults
 
-		// # Show Lightbox
+  var defaults = {
+  	transition: 350,				// transition duration
+  	spin : {
+  		top: 50,
+  		left: 50
+  	}								// default for spin
+  }
 
-	}
+  /* Constructor */
+  function RGlightbox(options) {
+  	if (typeof this == 'undefined') return new RGlightbox(options)
+  	// extends defaults
+    this.opts = merge(options || {}, RGlightbox.defaults, defaults)
+  }
 
-	// # Init Core
+  // Global defaults that override the built-ins:
+  Spinner.defaults = {}
 
-	_rglightbox.prototype.constructor = _rglightbox;
+  /**
+   * Build Prototype
+   */
 
-	_rglightbox.prototype._init = function() {
+  merge(RGlightbox.prototype, {
 
-		//build backdrop
-		// this._buildBD();
+  	show : function() {
+  		var _that = this;
+  		console.log('se ha llamado show');
+  		// show backdrop
+  		this.showDB(function(){
+				// show lightbox container when BD loads
+				_that.showLB();
+				_that.loading();
+			});
+  	},
 
-		//build lightbox
-		// this._build();
+	  /**
+	   * Utilities
+	   */
 
-		console.log('se inicio :3');
-	}
+	  // build lightbox
+	  buildLB : function() {
+	  	var newRGlightbox = window.document.getElementById('rglightbox');
 
-	// # Show Lightbox
+			if (!newRGlightbox) {
 
-	_rglightbox.prototype._show = function() {
-		var _that = this;
-		// show BD
-		this._showBD(function(){
-			// show lightbox container when BD loads
-			_that._showLB()
-		});
-	}
+				newRGlightbox = document.createElement('div');
 
-	// # Show LB
+				var newController = document.createElement('div'),
+				newInfo           = document.createElement('div');
 
-	_rglightbox.prototype._showLB = function() {
-		var _lightbox = this._build();
-		window.setTimeout(function() {
-			_lightbox.className = _lightbox.className + ' rg-fadein';
-		}, 10);
-		console.log('ahora se mostrara el lb', _lightbox);
-	}
+				// rglightbox
+				newRGlightbox.className = 'rglightbox rg-fade';
+				newRGlightbox.id        = 'rglightbox';
 
-	// # Show BackDrop
-
-	_rglightbox.prototype._showBD = function(callback) {
-		var _that = this;
-
-		console.log('el valor del fading es', this.fading);
-
-		var _backdrop = this._buildBD();
-
-		// show
-		window.setTimeout(function() {
-			// animate
-			_backdrop.className = _backdrop.className + ' rg-fadein';
-			if (callback && typeof callback == 'function') {
-				console.log('se correra', callback);
-				console.log('en.. ', _that.fading);
-				window.setTimeout(callback, _that.fading);
 			}
-		}, 10);
 
-	}
-
-	// # Build Lightbox
-
-	_rglightbox.prototype._build = function() {
-		'use strict';
-
-		var newRGlightbox = window.document.getElementById('rglightbox');
-
-		if (!newRGlightbox) {
-
-			console.log('se construira');
-
-			newRGlightbox = document.createElement('div');
-
-			var newContainer = document.createElement('div'),
-			newController    = document.createElement('div'),
-			newInfo          = document.createElement('div');
-
-			// rglightbox
-
-			newRGlightbox.className = 'rglightbox rg-fade';
-			newRGlightbox.id        = 'rglightbox';
-
+			return newRGlightbox;
+	  },
+	  // show lightbox
+	  showLB : function() {
+	  	var newRGlightbox = this.buildLB();
 			// container
-
-			newContainer.id = 'rglightbox-container';
+			var newContainer       = document.getElementById('rglightbox-container') || document.createElement('div');
+			newContainer.id        = 'rglightbox-container';
 			newContainer.className = 'rglightbox-container';
 			newContainer.appendChild(newRGlightbox);
-
-			// add lightbox structure to document
-
+	  	// add lightbox container structure to document
 			document.getElementsByTagName("body")[ 0 ].appendChild(newContainer);
+			// show lightbox
+			window.setTimeout(function() {
+				newRGlightbox.className = newRGlightbox.className + ' rg-fadein';
+			}, 10);
+	  },
 
-		}
+	  // build backdrop
+	  buildBD : function() {
+	  	var _backdrop = document.getElementById('rglightbox-backdrop');
 
-		return newRGlightbox;
-	}
+			if (!_backdrop) {
+				// if backdrop doesn't exist then create it
+				_backdrop           = document.createElement('div');
+				_backdrop.className = 'rglightbox-backdrop rg-fade';
+				_backdrop.id        = 'rglightbox-backdrop';
+			}
+			return _backdrop;
+	  },
 
-	// # Build backdrop
-
-	_rglightbox.prototype._buildBD = function() {
-		var _backdrop       = document.getElementById('rglightbox-backdrop');
-
-		if (!_backdrop) {
-			// if backdrop doesn't exist then create it
-			console.log('no existe backdrop crearlo');
-			_backdrop           = document.createElement('div');
-			_backdrop.className = 'rglightbox-backdrop rg-fade';
-			_backdrop.id        = 'rglightbox-backdrop';
+	  // show backdrop
+	  showDB : function(callback) {
+	  	var _backdrop = this.buildBD(),
+	  	_that = this;
 			// insert in document
 			document.getElementsByTagName('body')[0].appendChild(_backdrop);
-		}
-		return _backdrop;
-	}
+			// show
+			window.setTimeout(function() {
+				// animate
+				_backdrop.className = _backdrop.className + ' rg-fadein';
+				if (callback && typeof callback == 'function') {
+					window.setTimeout(callback, _that.opts.transition);
+				}
+			}, 10);
+	  },
 
-	// # Create a single view
+	  // resize LB
+	  resize: function(w, h) {
+			var w = typeof w == 'number' ? w : parseInt(w, 10),
+			y     = typeof y == 'number' ? h : parseInt(h, 10),
+			_lb   = this.buildLB(),
+			padding = parseInt( window.getComputedStyle(_lb).getPropertyValue('padding'), 10) || 0;
+			// set size
+			_lb.style.width  = w + 'px';
+			_lb.style.height = h + 'px';
+			// center lb
+			_lb.style.position   = 'absolute';
+			_lb.style.left       = '50%';
+			_lb.style.top        = '50%';
+			_lb.style.marginLeft = ((w / -2) -padding) + 'px';
+			_lb.style.marginTop  = ((h / -2) -padding) + 'px';
+	  },
 
-	_rglightbox.prototype.create = function(first_argument) {
-		'use strict';
-		// body...
-	};
+	  // loading
+	  loading: function(status) {
+			var _lb   = this.buildLB();
+			// resize LB
+			this.resize(100, 100);
+			// set spinner
+			var spinner   = new Spinner(this.opts.spin).spin(_lb);
+	  }
+  });
 
-	// # Create a Gallery
 
-	_rglightbox.prototype.createGallery = function(first_argument) {
-		'use strict';
-		// body...
-	};
-
-	// # Constructor
-
-	_rglightbox.prototype.constructor = _rglightbox;
-
-	// define global
-	root.RGlightbox = _rglightbox;
-})(this);
+  return RGlightbox;
+});
