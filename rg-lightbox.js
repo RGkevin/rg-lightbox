1e6a8311c8b77f4bbb35a1c03dd29f115cf41b0a
Kevin Lopez <rebeliongraficks@gmail.com>
2013-10-06 08:49:31 -0600

    navigation

--- rg-lightbox.js
+++ rg-lightbox.js
@@ -9,13 +9,19 @@
 (function(root, factory) {
 
 	/* CommonJS */
-  if (typeof exports == 'object')  module.exports = factory()
-
+  if (typeof exports == 'object')  {
+  	module.exports = factory()
+	}
   /* AMD module */
-  else if (typeof define == 'function' && define.amd) define(['spin'], factory)
-
+  else if (typeof define == 'function' && define.amd) {
+  	define(['spin'], factory)
+  }
   /* Browser global */
-  else root.RGlightbox = factory( root.Spinner )
+  else {
+  	root.RGlightbox = factory( root.Spinner );
+  	/*Auto Load RGlightbox for non AMD users*/
+  	// root.RGlightboxElements = new RGlightbox(window.document.querySelectorAll(['[data-lightbox]']));
+  }
 
 })
 (this, function(Spinner) {
@@ -33,6 +39,50 @@
     return obj
   }
 
+  /**
+   * Prevent Default for events
+   */
+
+  function preventDefault(event) {
+		if (event.preventDefault) {
+			event.preventDefault();
+		} else {
+			event.returnValue = false;
+		}
+	}
+
+	/**
+   * Filter method for Array *IE fix
+   */
+
+	if (!Array.prototype.filter) {
+	  Array.prototype.filter = function(fun /*, thisp*/) {
+	    'use strict';
+
+	    if (!this) {
+	      throw new TypeError();
+	    }
+
+	    var objects = Object(this);
+	    var len = objects.length >>> 0;
+	    if (typeof fun !== 'function') {
+	      throw new TypeError();
+	    }
+
+	    var res = [];
+	    var thisp = arguments[1];
+	    for (var i in objects) {
+	      if (objects.hasOwnProperty(i)) {
+	        if (fun.call(thisp, objects[i], i, objects)) {
+	          res.push(objects[i]);
+	        }
+	      }
+	    }
+
+	    return res;
+	  };
+	}
+
   // Built-in defaults
 
   var defaults = {
@@ -44,10 +94,20 @@
   }
 
   /* Constructor */
-  function RGlightbox(options) {
-  	if (typeof this == 'undefined') return new RGlightbox(options)
+  function RGlightbox() {
+  	if (typeof this == 'undefined') return new RGlightbox(arguments)
   	// extends defaults
-    this.opts = merge(options || {}, RGlightbox.defaults, defaults)
+    // check if the arguments passed are HTML elements or and data object
+  	if (arguments[0].length) {
+  		this.elements = arguments[0];
+	    this.opts = merge(arguments[1] || {}, RGlightbox.defaults, defaults);
+  	} else {
+  		this.elements = [];
+	    this.opts = merge(arguments[0] || {}, RGlightbox.defaults, defaults)
+  	}
+  	// console.log(this.opts, this.elements);
+  	// start RGlightbox
+  	this.init();
   }
 
   // Global defaults that override the built-ins:
@@ -59,15 +119,51 @@
 
   merge(RGlightbox.prototype, {
 
-  	show : function() {
-  		var _that = this;
-  		console.log('se ha llamado show');
-  		// show backdrop
-  		this.showDB(function(){
-				// show lightbox container when BD loads
-				_that.showLB();
-				_that.loading();
-			});
+  	// start everything
+  	init: function (){
+  		console.log('start everything');
+  		// create galleries
+  		this.createGalleries(this.elements);
+  		// bind events
+  		this.bind(this.elements);
+  	},
+
+  	// show gallery
+  	showGallery: function(gallery, url) {
+			var _that               = this,
+			_gallery                = this.getGalleryDataByName(gallery);
+			_gallery._startImage    = _gallery.urls.indexOf(url);
+			_gallery._galleryLength = _gallery.urls.length - 1;
+
+  		console.log('se mostrara', _gallery._startImage, 'de', _gallery._galleryLength);
+  		// start lightbox
+  		this.showLB();
+  		// build controlls
+  		this.buildControlls();
+  		// load firts image
+  		this.showImageOfGallery(_gallery.urls, _gallery._startImage);
+  		// set controlls events
+  		this.bindControlls(gallery);
+  	},
+
+  	// close lightbox
+  	close: function() {
+  		var _that = this,
+  		_lightbox = this.buildLB(),
+  		_lbContainer = window.document.getElementById('rglightbox-container'),
+  		_lbBackdrop = window.document.getElementById('rglightbox-backdrop'),
+  		_body = window.document.getElementsByTagName('body')[0],
+  		_newClassName = _lightbox.className.replace(' rg-fadein', '')
+  		_lightbox.className = _newClassName;
+  		window.setTimeout(function() {
+  			_body.removeChild(_lbContainer);
+  			_newClassName = _lbBackdrop.className.replace(' rg-fadein', '');
+  			_lbBackdrop.className = _newClassName;
+  			window.setTimeout(function() {
+	  			_body.removeChild(_lbBackdrop);
+  			}, _that.opts.transition);
+  			// _body.removeChild(_lbBackdrop);
+  		}, this.opts.transition);
   	},
 
 	  /**
@@ -95,18 +191,19 @@
 	  },
 	  // show lightbox
 	  showLB : function() {
-	  	var newRGlightbox = this.buildLB();
+			var newRGlightbox      = this.buildLB(),
 			// container
-			var newContainer       = document.getElementById('rglightbox-container') || document.createElement('div');
+			newContainer           = document.getElementById('rglightbox-container') || document.createElement('div');
 			newContainer.id        = 'rglightbox-container';
 			newContainer.className = 'rglightbox-container';
 			newContainer.appendChild(newRGlightbox);
 	  	// add lightbox container structure to document
 			document.getElementsByTagName("body")[ 0 ].appendChild(newContainer);
-			// show lightbox
-			window.setTimeout(function() {
+  		// show backdrop
+  		this.showDB(function(){
+				// show lightbox when BD loads
 				newRGlightbox.className = newRGlightbox.className + ' rg-fadein';
-			}, 10);
+			});
 	  },
 
 	  // build backdrop
@@ -138,15 +235,136 @@
 			}, 10);
 	  },
 
+	  // build controls
+	  buildControlls: function(){
+			var _lightbox = this.buildLB(),
+			_left = _lightbox.getElementsByClassName('rglightbox-left')[0],
+			_right        = _lightbox.getElementsByClassName('rglightbox-right')[0],
+			_close        = _lightbox.getElementsByClassName('rglightbox-close')[0];
+
+			if (!_left) {
+				// creating left control
+				_left         = window.document.createElement('a');
+				_left.textContent  = 'left';
+				_left.className    = 'rglightbox-left';
+				_lightbox.appendChild(_left);
+			}
+
+			if (!_right) {
+				_right = window.document.createElement('a');
+				// creating right control
+				_right.textContent = 'right';
+				_right.className   = 'rglightbox-right';
+				_lightbox.appendChild(_right);
+			}
+			if (!_close) {
+				_close = window.document.createElement('a');
+				// creating close control
+				_close.textContent = '×'; //×
+				_close.className   = 'rglightbox-close';
+				_close.title       = 'Close';
+				_lightbox.appendChild(_close);
+			}
+			return [_left, _right, _close];
+	  },
+
+	  // bind events to controlls
+	  bindControlls: function(gallery) {
+			var _that = this,
+			_buttons  = this.buildControlls(),
+			_prevBtn  = _buttons[0],
+			_nextBtn  = _buttons[1],
+			_closeBtn = _buttons[2];
+	  	// set prev event
+	  	_prevBtn.addEventListener('click', function(event) {
+	  		var _gallery = _that.getGalleryDataByName(gallery),
+	  		_newStartImage;
+	  		if (_gallery._startImage > 0) {
+		  		_newStartImage = --_that.getGalleryDataByName(gallery)._startImage;
+	  		} else {
+	  			_newStartImage = _that.getGalleryDataByName(gallery)._startImage = _gallery._galleryLength;
+	  		}
+	  		_that.showImageOfGallery(_gallery.urls, _newStartImage);
+	  		preventDefault(event);
+	  	});
+
+	  	// set next event
+	  	_nextBtn.addEventListener('click', function(event) {
+	  		var _gallery = _that.getGalleryDataByName(gallery),
+	  		_newStartImage;
+	  		if (_gallery._startImage < _gallery._galleryLength) {
+		  		_newStartImage = ++_that.getGalleryDataByName(gallery)._startImage;
+	  		} else {
+	  			_newStartImage = _that.getGalleryDataByName(gallery)._startImage = 0;
+	  		}
+	  		_that.showImageOfGallery(_gallery.urls, _newStartImage);
+	  		preventDefault(event);
+	  	});
+
+	  	// set close event
+	  	_closeBtn.addEventListener('click', function(event) {
+	  		console.log('se cerrara el lightbox');
+	  		_that.close();
+	  		preventDefault(event);
+	  	});
+	  },
+
+	  // show Image
+  	showImage: function(url) {
+  		console.log('se motrara', url);
+  	},
+
+	  // show image of gallery
+		showImageOfGallery:function(urls, index) {
+			var _that = this;
+			// remove current image if exists
+			this.removeCurrentImage(function(){
+				_that.loading().start(); // start loading state
+				// build new image
+				var image = window.document.createElement('img');
+				image.addEventListener('load', function(event) {
+					_that.loading().stop(); // stop loadin state
+					_that.resize(this.width, this.height); // resize lightbox
+					_that.buildLB().appendChild(this); // add image
+				});
+				image.src = urls[index];
+			});
+		},
+
+		// remove current image
+		removeCurrentImage: function(callback){
+			var _lightbox = this.buildLB(),
+			_image = _lightbox.getElementsByTagName('img')[0];
+			// if image exists
+			if (_image) {
+				// -- add fade animation
+				_image.className = 'rg-fade';
+				// remove
+				window.setTimeout(function(){
+					_lightbox.removeChild(_image);
+					if (typeof callback === 'function') callback();
+				}, this.opts.transition);
+			} else {
+				if (typeof callback === 'function') callback();
+			}
+		},
+
 	  // resize LB
 	  resize: function(w, h) {
 			var w = typeof w == 'number' ? w : parseInt(w, 10),
 			y     = typeof y == 'number' ? h : parseInt(h, 10),
 			_lb   = this.buildLB(),
-			padding = parseInt( window.getComputedStyle(_lb).getPropertyValue('padding'), 10) || 0;
+			_lbStyles = window.getComputedStyle(_lb),
+			padding = parseInt( _lbStyles.getPropertyValue('padding'), 10) || 0,
+			sizing = _lbStyles.getPropertyValue('box-sizing') || null;
 			// set size
-			_lb.style.width  = w + 'px';
-			_lb.style.height = h + 'px';
+			if (!sizing || sizing !== 'border-box' ) {
+				_lb.style.width  = w + 'px';
+				_lb.style.height = h + 'px';
+			} else {
+				_lb.style.width  = w + (padding * 2) + 'px';
+				_lb.style.height = h + (padding * 2) + 'px';
+			}
 			// center lb
 			_lb.style.position   = 'absolute';
 			_lb.style.left       = '50%';
@@ -156,12 +374,92 @@
 	  },
 
 	  // loading
-	  loading: function(status) {
-			var _lb   = this.buildLB();
-			// resize LB
-			this.resize(100, 100);
-			// set spinner
-			var spinner   = new Spinner(this.opts.spin).spin(_lb);
+	  loading: function() {
+			var _that = this,
+			_lb = _that.buildLB();
+			return {
+				start: function(){
+					// set loading class
+					_lb.className = _lb.className + ' rglightbox-loading';
+					// resize LB
+					_that.resize(100, 100);
+					// set spinner
+					var spinner   = _lb.getElementsByClassName('spinner')[0] ? null : new Spinner(_that.opts.spin).spin(_lb);
+				},
+				stop: function() {
+					var _newClassName = _lb.className.replace(' rglightbox-loading', ' ');
+					console.log('se borrara spinner', _lb.getElementsByClassName('spinner')[0]);
+					if( _lb.getElementsByClassName('spinner')[0] ) {
+						_lb.removeChild( _lb.getElementsByClassName('spinner')[0] || [] ); // remove spinner
+					}
+					_lb.className = _newClassName; // remove loading class name
+				}
+			}
+	  },
+
+	  // create galleries
+	  createGalleries: function(elements){
+			var galleries  = [];
+			this.galleries = [];
+		  // get array of gallery names
+	  	for (var i = 0, len = elements.length; i < len; i++) {
+	  		if(elements[i].getAttribute('data-lightbox') !== "") galleries.push( elements[i].getAttribute('data-lightbox') || '' );
+	  	}
+	  	// remove duplicated elements
+	  	galleries = galleries.filter(function(elem, pos, self) {
+			    return self.indexOf(elem) == pos;
+			});
+			// return array of urls based on gallery name
+			function getUrls(gallery){
+				var _urls = []; // temp urls variable
+
+		  	for (var i = 0, len = elements.length; i < len; i++) {
+		  		if (elements[i].getAttribute('data-lightbox') === gallery) _urls.push(elements[i].href);
+		  	}
+
+				return _urls;
+			}
+			// storing galleries
+			for (var i = 0, len = galleries.length; i < len; i++) {
+				// add new gallery
+				this.galleries.push({
+					name: galleries[i],
+					urls: getUrls(galleries[i])
+				});
+			}
+			console.log(this.galleries);
+	  },
+
+	  // get gallery
+	  getGalleryDataByName: function(gallery) {
+	  	var _that = this;
+  		for (var i = 0, len = this.galleries.length; i < len; i++) {
+  			if(this.galleries[i].name === gallery) {
+  				return this.galleries[i];
+  			}
+  		}
+  		return null;
+	  },
+
+	  // bind events
+	  bind:function(elements) {
+	  	var _that = this;
+
+	  	// bind click event
+	  	for (var i = 0, len = elements.length; i < len; i++) {
+	  		elements[i].addEventListener('click', function(event) {
+	  			console.log('se clico', this);
+
+	  			if (this.getAttribute('data-lightbox') !== '') {
+	  				// show gallery
+	  				_that.showGallery(this.getAttribute('data-lightbox'), this.href || '');
+	  			} else {
+	  				_that.showImage(this.href);
+	  			}
+
+	  			preventDefault(event);
+	  		}, false);
+	  	}
 	  }
   });
 
